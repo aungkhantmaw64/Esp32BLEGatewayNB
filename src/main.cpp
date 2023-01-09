a4d9b9e5eb645be9cf69fa44f4532870f08f72a7
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-01-09 17:40:47 +0700

    Add beacon scanning and gps reading tasks

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,79 @@
    Ported to Arduino ESP32 by Evandro Copercini
 */
 #include <Arduino.h>
+#include "YACL.h"
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
+
 #include "observer_module.h"
+#include "gps_module.h"
 
 #define BLE_SCAN_INTERVALMS 100
 #define BLE_SCAN_WINDOW 90
 #define BLE_SCAN_TIME 5
 
-ObserverModule observerModule = ObserverModule(BLE_SCAN_INTERVALMS,
-                                               BLE_SCAN_WINDOW,
-                                               BLE_SCAN_TIME);
+#define BLE_TASK_BLOCKED_PERIOD_MS 1000
+#define BLE_TASK_STACK_SIZE 2000
+
+#define GPS_TASK_BLOCKED_PERIOD_MS 1000
+#define GPS_TASK_STACK_SIZE 2000
+#define GPS_RX 18
+#define GPS_TX 19
+#define GPS_BAUDRATE 9600
+
+static ObserverModule observerModule(BLE_SCAN_INTERVALMS,
+                                     BLE_SCAN_WINDOW,
+                                     BLE_SCAN_TIME);
+
+static GpsModule gpsModule(GPS_RX, GPS_TX);
+
+static TaskHandle_t xBeaconTaskHandle = NULL;
+static TaskHandle_t xGpsTaskHandle = NULL;
+
+static void vBeaconScanTask(void *pvParameters)
+{
+  while (1)
+  {
+    observerModule.poll();
+    vTaskDelay(BLE_TASK_BLOCKED_PERIOD_MS / portTICK_RATE_MS);
+  }
+  vTaskDelete(NULL);
+}
+
+static void vGPSReadTask(void *pvParameters)
+{
+  while (1)
+  {
+    gpsModule.poll();
+    vTaskDelay(GPS_TASK_BLOCKED_PERIOD_MS / portTICK_RATE_MS);
+  }
+  vTaskDelete(NULL);
+}
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Scanning...");
   observerModule.begin();
+  gpsModule.begin(GPS_BAUDRATE);
+
+  xTaskCreate(vBeaconScanTask,
+              "BeaconScan",
+              BLE_TASK_STACK_SIZE,
+              NULL,
+              0,
+              &xBeaconTaskHandle);
+
+  xTaskCreate(vGPSReadTask,
+              "GpsRead",
+              GPS_TASK_STACK_SIZE,
+              NULL,
+              0,
+              &xGpsTaskHandle);
 }
 
 void loop()
 {
-  observerModule.poll();
-  delay(2000);
 }
